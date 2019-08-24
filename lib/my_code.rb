841191ddf5606e3740d2ac7e0378ebb802a569d7
Karina Macancela <karymacancela@gmail.com>
2019-08-24 21:40:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -36,7 +36,7 @@ end
 def reduce_to_total(source_array, starting_point)
   total = 0
   if starting_point == nil
-    (source_array.length - 1).
+    (source_array.length - 1).times dp
   else
     
   end
