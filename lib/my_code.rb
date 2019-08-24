91576c44824e44bcaa45c32839e3ce6e350b9dfd
Karina Macancela <karymacancela@gmail.com>
2019-08-24 21:33:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,12 @@ def map_to_negativize(source_array)
   new_array
 end
 
+def map_to_no_change(source_array)
+  source_array
+end
+
+def map_to_double(source_array)
+end
+
+def map_to_square(source_array)
+end
