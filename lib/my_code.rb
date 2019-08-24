db0a5cde488a04ac94db0379a623c703ab2a8c44
Karina Macancela <karymacancela@gmail.com>
2019-08-24 21:35:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,11 +9,26 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
-  source_array
+  new_array = source_array
+  new_array
 end
 
 def map_to_double(source_array)
+  counter = 0
+  new_array = []
+  while counter < source_array.length
+    new_array << source_array[counter] * 2
+    counter += 1
+  end
+  new_array
 end
 
 def map_to_square(source_array)
+  counter = 0
+  new_array = []
+  while counter < source_array.length
+    new_array << source_array[counter] ** 2
+    counter += 1
+  end
+  new_array
 end
