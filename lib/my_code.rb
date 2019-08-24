68b681c59111bd2d47b6d2735aac7f63948ee5b4
Karina Macancela <karymacancela@gmail.com>
2019-08-24 21:38:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,8 +34,9 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
+  total = 0
   if starting_point == nil
-    
+    (source_array.length - 1).
   else
     
   end
